6d2d08da033f80238bfc4fd989b96d3da45390ba
jessberg <jessberg@users.noreply.github.com>
2022-12-08 13:48:18 -0500

    Update vm_setup.sh

--- vm_setup.sh
+++ vm_setup.sh
@@ -1,7 +1,6 @@
 sudo apt-get install git
 git clone https://github.com/dyn-tracing/microservices_env.git
 cd microservices_env
-./tools/setup.sh
 rmdir trace_storage
 git clone https://github.com/dyn-tracing/trace_storage.git
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
